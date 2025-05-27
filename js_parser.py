64a46be6b80e530e74cb2556a3bf251cc0192cab
Gaurav Singh Rawat <144826894+Gauravrawat2004@users.noreply.github.com>
2025-05-27 14:35:26 +0530

    Add files via upload

---
+++ js_parser.py
@@ -0,0 +1,96 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+# --- JS Lexer ---
+class JSLexerConfig:
+    tokens = ('VAR', 'IF', 'CONSOLE', 'DOT', 'LOG', 'ID', 'NUMBER', 'EQUALS', 'LESS', 'SEMI', 'LPAREN', 'RPAREN')
+
+    t_ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t_NUMBER = r'\d+'
+    t_EQUALS = r'='
+    t_LESS = r'<'
+    t_SEMI = r';'
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_ignore = ' \t\n'
+
+    def t_VAR(self, t):
+        r'var'
+        return t
+
+    def t_IF(self, t):
+        r'if'
+        return t
+
+    def t_CONSOLE(self, t):
+        r'console'
+        return t
+
+    def t_DOT(self, t):
+        r'\.'
+        return t
+
+    def t_LOG(self, t):
+        r'log'
+        return t
+
+    def t_error(self, t):
+        print(f"Illegal character: {t.value[0]}")
+        t.lexer.skip(1)
+
+    def build(self):
+        return lex.lex(module=self)
+
+# --- JS Parser ---
+class JSParserConfig:
+    def __init__(self):
+        self.tokens = JSLexerConfig.tokens
+        self.precedence = (
+            ('left', 'LESS'),
+            ('left', 'EQUALS'),
+            ('left', 'DOT'),
+        )
+
+    def p_program(self, p):
+        '''program : statements'''
+        p[0] = {"type": "Program", "body": p[1]}
+
+    def p_statements(self, p):
+        '''statements : statement
+                      | statements statement
+                      | '''
+        p[0] = [p[1]] if len(p) == 2 else p[1] + [p[2]] if len(p) == 3 else []
+
+    def p_statement_declaration(self, p):
+        '''statement : VAR ID EQUALS NUMBER SEMI'''
+        p[0] = {"type": "Declaration", "var": p[2], "value": p[4]}
+
+    def p_statement_if(self, p):
+        '''statement : IF LPAREN ID LESS NUMBER RPAREN statement'''
+        p[0] = {"type": "If", "condition": {"left": p[3], "op": p[4], "right": p[5]}, "body": [p[7]]}
+
+    def p_statement_print(self, p):
+        '''statement : CONSOLE DOT LOG LPAREN ID RPAREN SEMI'''
+        p[0] = {"type": "Print", "value": {"type": "Var", "name": p[5]}}
+
+    def p_error(self, p):
+        if p:
+            raise Exception(f"Syntax error at '{p.value}' on line {p.lineno}")
+        else:
+            raise Exception("Syntax error: Unexpected end of input")
+
+def get_js_parser():
+    lexer = JSLexerConfig().build()
+    parser = yacc.yacc(module=JSParserConfig(), debug=True)
+    return lexer, parser
+
+# Debug lexer
+if __name__ == "__main__":
+    lexer = JSLexerConfig().build()
+    code = """
+    var x = 5;
+    if (x < 10) console.log(x);
+    """
+    lexer.input(code)
+    for token in lexer:
+        print(token)
